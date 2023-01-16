e78f383f7f4079556feefee5fb8e9b478cf6d7ab
sn3aky <konradlab01@gmail.com>
2023-01-16 20:02:56 +0100

    mqtt

--- App.js
+++ App.js
@@ -1,22 +1,53 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Amplify, PubSub } from 'aws-amplify';
+import { Amplify, PubSub, Auth } from 'aws-amplify';
 import { AWSIoTProvider } from '@aws-amplify/pubsub';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import awsconfig from './src/aws-exports'
 
 Amplify.configure(awsconfig)
 
+Amplify.addPluggable(
+  new AWSIoTProvider({
+    aws_pubsub_region: 'eu-central-1',
+    aws_pubsub_endpoint:
+      'ae8md3vbbgnl9-ats.iot.eu-central-1.amazonaws.com'
+  })
+);
+
 function App() {
+
+  useEffect(() => {
+
+    Auth.currentCredentials().then((info) => {
+      const cognitoIdentityId = info.identityId;
+      console.log('cognitoIdentityId', cognitoIdentityId);
+    });
+
+
+    PubSub.subscribe('sdkTest/sub').subscribe({
+      next: data => console.log('Message received', data),
+      error: error => console.error(error),
+      complete: () => console.log('Done'),
+    });
+
+    // PubSub.publish('myTopic1', { msg: 'Hello to all subscribers!' });
+
+
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 export default withAuthenticator(App, {
+  includeGreetings: true,
   signUpConfig: {
     hiddenDefaults: ['phone_number']
   }
@@ -25,7 +56,7 @@ export default withAuthenticator(App, {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
