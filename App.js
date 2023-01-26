777a03e97bbf87ab058fe9ff14ddf9643e1d535d
sn3aky <konradlab01@gmail.com>
2023-01-26 03:27:23 +0100

    mqtt

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Amplify, PubSub, Auth } from 'aws-amplify';
 import { AWSIoTProvider } from '@aws-amplify/pubsub';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import awsconfig from './src/aws-exports'
+import {
+  LineChart,
+} from "react-native-chart-kit";
+
+
 
 Amplify.configure({
   ...awsconfig,
@@ -17,11 +22,13 @@ Amplify.addPluggable(
   new AWSIoTProvider({
     aws_pubsub_region: 'eu-central-1',
     aws_pubsub_endpoint:
-      'ae8md3vbbgnl9-ats.iot.eu-central-1.amazonaws.com'
+      'wss://ae8md3vbbgnl9-ats.iot.eu-central-1.amazonaws.com/mqtt'
   })
 );
 
 function App() {
+
+  [messages, setMessages] = useState([0, 1])
 
   useEffect(() => {
 
@@ -32,21 +39,57 @@ function App() {
 
 
     PubSub.subscribe('sdkTest/sub').subscribe({
-      next: data => console.log('Message received', data),
+      next: data => setMessages([...messages, data.value.Temperature]),
       error: error => console.error(error),
-      complete: () => console.log('Done'),
+      complete: () => console.log('Message received'),
     });
-    // PubSub.publish('sdkTest/pub', { msg: 'Hello to all subscribers!' });
 
   }, [])
 
 
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
       <Button title="send" onPress={() => {
         PubSub.publish('sdkTest/pub', { msg: 'Hello to all subscribers!' });
       }} />
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: messages
+            }
+          ]
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisLabel="ºC"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
+      <ScrollView>
+        {messages.map((msg, index) => <Text key={index}>{msg} </Text>)}
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
